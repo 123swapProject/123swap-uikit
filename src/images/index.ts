ec17f5b4efe2e6768b75b35c5d95e5a91c6528af
onetwothreeswap <onetwothreeswap@gmail.com>
2021-06-22 11:43:08 +0300

    Changed logos

--- src/images/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as logo_white } from "./123swap_white.png";
-export { default as logo_black } from "./123swap_black.png";
-export { default as logo_round } from "./123swap_round.png";
