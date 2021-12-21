8461eca95e0499366b55879278063893a6857dd7
Andriuskevicus <andriuskevicius.ernestas@gmail.com>
2021-12-21 21:27:23 +0200

    Changed UIKIT version

--- src/widgets/Menu/icons/Logo.tsx
+++ src/widgets/Menu/icons/Logo.tsx
@@ -1,16 +1,24 @@
 import React from "react";
+import styled from "styled-components";
 import { SvgProps } from "../../../components/Svg/types";
 import { LogotypeIcon } from ".";
 
 interface LogoProps extends SvgProps {
   isDark: boolean;
 }
+const StyledLogoWrapper = styled.div`
+  @media (max-width: 968px) {
+    display: none;
+  }
+`
 
 const Logo: React.FC<LogoProps> = ({ isDark, ...props }) => {
   
   const iconprops = { style: {height: 50, width: "auto"} }
   return (
+    <StyledLogoWrapper>
       <LogotypeIcon {...iconprops} />
+    </StyledLogoWrapper>
   );
 };
 
